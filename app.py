d064ac7e14c747c46157ff67c3218035d9a58cbb
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-09-03 02:44:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ import numpy as np
 x = ["A", "K", "Q", "J","T", "9", "8" , "7" , "6" , "5" , "4" , "3" , "2"]
 y = ["A", "K", "Q", "J","T", "9", "8" , "7" , "6" , "5" , "4" , "3" , "2"]
 
-st.sidebar.write('_'*20)
 c_1 = st.sidebar.radio("c_1",(x))
 c_2 = st.sidebar.radio("c_2",(x))
-st.sidebar.write('_'*20)
+Suit = st.sidebar.radio("Suit",(o , s))
 
 Z = {"A":0, "K":1, "Q":2, "J":3,"T":4, "9":5, "8":6, "7":7, "6":8, "5":9, "4":10, "3":11, "2":12}
 A = 0.7  ; B = 0.6 ; C =  0.5 ; D= 0.4  ; E = 0.3 ; F = 0.2  ; G = 0.1  ; H =  0.0
@@ -34,7 +33,7 @@ data = np.array([
 [F, G, G, H, H, H, H, H, H, H, H, G, E],
 ])
 
-
+data[[c_1] ,[c_2]] = 1.
 df = pd.DataFrame(data)
 # c_1 = Z[c_1]
 # c_2 = Z[c_2]
