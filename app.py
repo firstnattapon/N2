fe4e3cd7bb874030d82d32ba0d5406218048ce54
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-09-03 05:20:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,35 +6,40 @@ import seaborn as sns
 import numpy as np
 # sns.set_style("whitegrid")
 
-
+@st.cache(suppress_st_warning=True)
+def preflop():
+    return  df = pd.read_pickle('./content/preflop.pickle')
+  
+df = preflop()
 x = ["A", "K", "Q", "J","T", "9", "8" , "7" , "6" , "5" , "4" , "3" , "2"]
-y = ["A", "K", "Q", "J","T", "9", "8" , "7" , "6" , "5" , "4" , "3" , "2"]
-
 c_1 = st.sidebar.radio("c_1",(x))
 c_2 = st.sidebar.radio("c_2",(x))
 Suit = st.sidebar.radio("Suit",(o , s))
 
-Z = {"A":0, "K":1, "Q":2, "J":3,"T":4, "9":5, "8":6, "7":7, "6":8, "5":9, "4":10, "3":11, "2":12}
-A = 0.7  ; B = 0.6 ; C =  0.5 ; D= 0.4  ; E = 0.3 ; F = 0.2  ; G = 0.1  ; H =  0.0
+h = c_1 + c_2 + Suit
+df = df[df['Human'] = h ]
 
-data = np.array([
-[A, A, B, C, D, E, F, F, F, F, F, F, F],
-[A, A, D, E, G, G, G, G, G, G, G, G, G],
-[B, D, A, E, G, G, G, G, G, G, G, G, G],
-[C, E, E, A, G, G, G, G, G, G, G, G, G],
-[D, G, G, F, B, G, G, G, G, G, G, G, G],
-[E, G, G, H, F, B, G, G, G, G, G, G, G],
-[F, G, G, H, H, F, C, G, G, G, G, G, G],
-[F, G, G, H, H, H, F, C, G, G, G, G, G],
-[F, G, G, H, H, H, H, F, D, G, G, G, G],
-[F, G, G, H, H, H, H, H, F, D, G, G, G],
-[F, G, G, H, H, H, H, H, H, F, E, G, G],
-[F, G, G,H, H, H, H, H, H, H, G, E,  G],
-[F, G, G, H, H, H, H, H, H, H, H, G, E],
-])
+# Z = {"A":0, "K":1, "Q":2, "J":3,"T":4, "9":5, "8":6, "7":7, "6":8, "5":9, "4":10, "3":11, "2":12}
+# A = 0.7  ; B = 0.6 ; C =  0.5 ; D= 0.4  ; E = 0.3 ; F = 0.2  ; G = 0.1  ; H =  0.0
 
-data[[c_1] ,[c_2]] = 1.
-df = pd.DataFrame(data)
+# data = np.array([
+# [A, A, B, C, D, E, F, F, F, F, F, F, F],
+# [A, A, D, E, G, G, G, G, G, G, G, G, G],
+# [B, D, A, E, G, G, G, G, G, G, G, G, G],
+# [C, E, E, A, G, G, G, G, G, G, G, G, G],
+# [D, G, G, F, B, G, G, G, G, G, G, G, G],
+# [E, G, G, H, F, B, G, G, G, G, G, G, G],
+# [F, G, G, H, H, F, C, G, G, G, G, G, G],
+# [F, G, G, H, H, H, F, C, G, G, G, G, G],
+# [F, G, G, H, H, H, H, F, D, G, G, G, G],
+# [F, G, G, H, H, H, H, H, F, D, G, G, G],
+# [F, G, G, H, H, H, H, H, H, F, E, G, G],
+# [F, G, G,H, H, H, H, H, H, H, G, E,  G],
+# [F, G, G, H, H, H, H, H, H, H, H, G, E],
+# ])
+
+# data[[c_1] ,[c_2]] = 1.
+# df = pd.DataFrame(data)
 # c_1 = Z[c_1]
 # c_2 = Z[c_2]
 # data[[c_1] ,[c_2]] = 1.
