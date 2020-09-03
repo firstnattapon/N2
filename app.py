c8dc5c677fcbaa48d9b5cb60f7c6bc9718c4fc31
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-09-03 14:55:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,22 +57,27 @@ st.sidebar.write(df)
 # st.pyplot()
 
 
+import pandas as pd
 
 import streamlit as st
 import streamlit.components.v1 as components
 
-_radio_button = components.declare_component(
-    "radio_button", url="https://soldier-n2.herokuapp.com/",
+_selectable_data_table = components.declare_component(
+    "selectable_data_table", url="http://localhost:3001",
 )
 
 
-def custom_radio_button(label, options, default, key=None):
-    return _radio_button(label=label, options=options, default=default, key=key)
+def selectable_data_table(data, key=None):
+    return _selectable_data_table(data=data, default=[], key=key)
 
 
-result = custom_radio_button(
-    "How many bats?",
-    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
-    default="one bat",
-)
-st.write("This many: %s" % result)
+raw_data = {
+    "First Name": ["Jason", "Molly", "Tina", "Jake", "Amy"],
+    "Last Name": ["Miller", "Jacobson", "Ali", "Milner", "Smith"],
+    "Age": [42, 52, 36, 24, 73],
+}
+df = pd.DataFrame(raw_data, columns=["First Name", "Last Name", "Age"])
+
+rows = selectable_data_table(df)
+if rows:
+    st.write("You have selected", rows)
