7bc6764e565ec2ce66f5288317971c29bd8d3545
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-09-07 22:58:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -387,7 +387,8 @@ if __name__ == '__main__':
     action = st.radio("action",("UN_OPENED" , "LIMPERS" ,"ONE_RAISE"))
     st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-    st.markdown("![f0796086a42dda21349a468c8866ff7c.png](https://www.img.in.th/images/f0796086a42dda21349a468c8866ff7c.png)")
+    st.markdown("![90dbb9ae25a0542d8876a74da01477a6.png](https://www.img.in.th/images/90dbb9ae25a0542d8876a74da01477a6.png)")
+    st.markdown("[![a607ec3f270aa7e759b723d935c5947a.png](https://www.img.in.th/images/a607ec3f270aa7e759b723d935c5947a.png)")
 
     y     = {'2':2/14 , '3':3/14, '4':4/14, '5':5/14, '6':6/14,'7':7/14,'8':8/14,'9':9/14,'T':10/14, 'J':11/14,'Q':12/14,'K':13/14,'A':14/14 , 'O':-1,'P':1,'S':2}
     z_1  = y[c_1]
