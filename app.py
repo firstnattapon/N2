f62ae32981fdcedd59ae83906fdc9f4bf379ff1a
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-09-10 02:27:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,15 +52,15 @@ if __name__ == '__main__':
 #         st.markdown("![90dbb9ae25a0542d8876a74da01477a6.png](https://www.img.in.th/images/90dbb9ae25a0542d8876a74da01477a6.png)")
 #         st.markdown("[![a607ec3f270aa7e759b723d935c5947a.png](https://www.img.in.th/images/a607ec3f270aa7e759b723d935c5947a.png)")
 
-    if st.checkbox("hiplot_preflop" , value = 0): 
-        if st.button("{}".format('Reset')):
-            session.run_id += 1
-        df = preflop()
-        data = df[['index', 'n_card1' , 'n_card2' , 's_suited'  , 'class_preflop', 'position' , 'action' , 'output_preflop' ,'ev']]
-        data = data.to_dict('r')
-        xp = hip.Experiment.from_iterable(data)
-        ret_val = xp.display_st(key=session.run_id)
-        st.markdown("hiplot returned " + json.dumps(ret_val))
+#     if st.checkbox("hiplot_preflop" , value = 0): 
+#         if st.button("{}".format('Reset')):
+#             session.run_id += 1
+#         df = preflop()
+#         data = df[['index', 'n_card1' , 'n_card2' , 's_suited'  , 'class_preflop', 'position' , 'action' , 'output_preflop' ,'ev']]
+#         data = data.to_dict('r')
+#         xp = hip.Experiment.from_iterable(data)
+#         ret_val = xp.display_st(key=session.run_id)
+#         st.markdown("hiplot returned " + json.dumps(ret_val))
         
 #_______________________________________________________________________________________________________
         
@@ -74,21 +74,19 @@ if __name__ == '__main__':
     b = x.board.unique()
     h = x.hit.unique()
    
-    op_p = st.selectbox('position',p)
-    op_b = st.selectbox('board',b)
+    op_p = st.radio('position',p)
+    op_b = st.radio('board',b)
     op_h = st.selectbox('hit',h)
-
-#     c_1 = st.radio("x",(x), key=session.run_id)
     
-    if st.checkbox("hiplot_postflop" , value = 0): 
-        if st.button("{}".format('Reset')):
-            session.run_id += 1
-        df_2 = postflop()
-        data_2 = df_2
-        data_2 = data_2.to_dict('r')
-        xp = hip.Experiment.from_iterable(data_2)
-        ret_val = xp.display_st(key=session.run_id)
-        st.markdown("hiplot returned " + json.dumps(ret_val))
+#     if st.checkbox("hiplot_postflop" , value = 0): 
+#         if st.button("{}".format('Reset')):
+#             session.run_id += 1
+#         df_2 = postflop()
+#         data_2 = df_2
+#         data_2 = data_2.to_dict('r')
+#         xp = hip.Experiment.from_iterable(data_2)
+#         ret_val = xp.display_st(key=session.run_id)
+#         st.markdown("hiplot returned " + json.dumps(ret_val))
         
 
 # import pandas as pd
